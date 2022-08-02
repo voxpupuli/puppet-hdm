10ae34802b24c9a627a1794fd12cd583b0f41d6d
Martin Alfke <ma@betadots.de>
2022-08-02 11:14:06 +0200

    start working on docker.

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -5,4 +5,44 @@
 # @example
 #   include hdm::docker
 class hdm::docker {
+  $directories = [
+    '/etc/hdm',
+    '/etc/hdm/certs',
+    '/etc/hdm/db',
+  ]
+  file { $directories:
+    ensure => directory,
+  }
+  file { '/etc/hdm/hdm.yml':
+    ensure  => file,
+    content => epp('modules/hdm/hdm.yml.epp'),
+  }
+  file { '/etc/hdm/database.yml':
+    ensure  => file,
+    content => epp('modules/hdm/database.yml'),
+  }
+  file { '/etc/hdm/db/development.sqlite3':
+    ensure => file,
+  }
+
+  docker::image { 'ghcr.io/betadots/hdm':
+    image_tag => $hdm::image_tag,
+  }
+
+  docker::run { 'hdm':
+    image    => "ghcr.io/betadots/hdm:${hdm::image_tag}",
+    env      => [
+      'TZ=Europe/Berlin',
+      "RAILS_DEVELOPMENT_HOSTS=${facts['fqdn']}",
+    ],
+    volumes  => [
+      '/etc/hdm/:/etc/hdm',
+      '/etc/puppetlabs/code:/etc/puppetlabs/code:ro',
+      '/etc/hdm/hdm.yml:/hdm/config/hdm.yml:ro',
+      '/etc/hdm/database.yml:/hdm/config/database.yml:ro',
+    ],
+    hostname => $facts['fqdn'],
+    ports    => [$hdm::port],
+    net      => 'host',
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,86 @@
 #
 # This class controls the installation of HDM
 #
+# @param method Select the installation method.
+#    Avalable methods: docker or puppet-ruby
+#    When using puppet-ruby we install bundler gem into 
+#    the puppet-agent ruby installation.
+#
+# @param port The port where HDM should run on
+#
+# @param puppetdb_settings A hash to provide information on how
+#   HDM can connect to puppetdb
+#   The following options are possible:
+#   Plain text (default)
+#     {
+#       'server'           => 'http://localhost:8080',
+#     }
+#   Using PE token:
+#     {
+#       'server'           => 'https://localhost:8081',
+#       'token'            => '/etc/hdm/puppetdb.token',
+#     }
+#   Using SSL cert:
+#     {
+#       'server'           => 'http://localhost:8081',
+#       'pem'              => {
+#         'key'            => '/etc/hdm/ssl.key',
+#         'cert'           => '/etc/hdm/ssl.cert',
+#         'ca_file'        => '/etc/hdm/ssl.ca',
+#       },
+#     }
+#
+# @param puppet_code_dir The path where HDM can find deployed 
+#   Puppet environments (similar to puppet config code_dir)
+#   defaults to '/etc/puppetlabs/code'
+#
+# @param allow_encryption Specify if HDM should use EYAML
+#   Needs HDM access to EYAML keys (public and private)
+#   Values for keys are taken from hiera.yaml file and can
+#   not be set individually.
+#
+# @param read_only Set to false if you want the ability to 
+#   change data via HDM webfrontend. WARNING!! untested!!!
+#   Changes are stored via GIT.
+#   Setting this to true also needs the git_data Hash parameter
+#
+# @param git_data Configure several settings related to the option
+#   to modify data via Webfrontend. WARNING!! untested!!
+#   Required Hash data:
+#     [
+#       {
+#         'datadir'        => 'modules/hieradata/data',
+#         'git_url'        => 'git@server:path/repo.git',
+#         'path_in_repo'   => 'data',
+#         'ssh_priv_key'   => '.ssh/id_rsa',
+#       }
+#     ]
+#
+# @param ldap_settings Config for LDAP integration
+#   Needs the following Hash:
+#     {
+#       'host'             => 'localhost',
+#       'port'             => 389,
+#       'base_dn'          => 'ou=hdm,dc=nodomain',
+#       'bind_dn'          => 'cn=admin,dc=nodomain',
+#       'bind_dn_password' => 'openldap',
+#     }
+#
+# @param hdm_hiera_config_file Set to another file if you 
+#   want HDM to not use hiera.yaml.
+#
 # @example
 #   include hdm
 class hdm (
-  Enum['docker', 'puppet-ruby'] $method = 'docker',
-  Stdlib::Port                  $port = 3000,
+  Enum['docker', 'puppet-ruby'] $method                = 'docker',
+  Stdlib::Port                  $port                  = 3000,
+  Hash                          $puppetdb_settings     = { 'server' => 'http://localhost:8080', },
+  Stdlib::Unixpath              $puppet_code_dir       = '/etc/puppetlabs/code',
+  String[1]                     $hdm_hiera_config_file = 'hiera.yaml',
+  Boolean                       $allow_encryption      = false,
+  Boolean                       $read_only             = true,
+  Optional[Hash]                $git_data              = undef,
+  Optional[Hash]                $ldap_settings         = undef,
 ){
   case $method {
     'docker': {
