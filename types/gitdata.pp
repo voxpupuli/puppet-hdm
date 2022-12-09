661f8fef5d7acc2f486d13f18fb820c48f84da8e
Martin Alfke <ma@betadots.de>
2022-12-09 15:01:33 +0100

    improve data types
    fixes #22

--- types/gitdata.pp
+++ types/gitdata.pp
@@ -1,11 +1,11 @@
 # @summary type to enforce git settings for HDM
 type Hdm::Gitdata = Array[
- Optional[Struct[
+ Struct[
     {
       datadir                => Stdlib::Unixpath,
       git_url                => String[1],
       path_in_repo           => String[1],
       Optional[ssh_priv_key] => String[1],
     }
-  ]]
+  ]
 ]
--- types/ldap_settings.pp
+++ types/ldap_settings.pp
@@ -1,11 +1,11 @@
 # @summary type to enforce ldap settings for HDM
 type Hdm::Ldap_settings = Struct[
   {
-    Optional[host]             => Stdlib::Host,
-    Optional[port]             => Stdlib::Port,
-    Optional[base_dn]          => String[1],
-    Optional[bind_dn]          => String[1],
-    Optional[bind_dn_password] => String[1],
-    'ldaps'                    => Boolean,
+    'host'             => Stdlib::Host,
+    'port'             => Stdlib::Port,
+    'base_dn'          => String[1],
+    'bind_dn'          => String[1],
+    'bind_dn_password' => Sensitive,
+    'ldaps'            => Boolean,
   }
 ]
