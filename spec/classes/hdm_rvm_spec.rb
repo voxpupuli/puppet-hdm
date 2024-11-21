9fa59e0aa16d01938882b9ef1e5e568c7201b79b
Martin Alfke <tuxmea@users.noreply.github.com>
2024-11-21 14:21:12 +0100

    Updates on config and spec (#98)
    * Updates on config and spec
    
    - on docker we must ensure that both paths are mounted (puppet_dir and
    puppet_code_dir)
    - add more spec tests
    
    * Fix rubocop
    
    * add spec test for image_tag

--- spec/classes/hdm_rvm_spec.rb
+++ spec/classes/hdm_rvm_spec.rb
@@ -14,6 +14,26 @@ describe 'hdm' do
       end
 
       it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_class('hdm::rvm') }
+      it { is_expected.to contain_rvm__system_user('hdm') }
+      it { is_expected.to contain_rvm_gem('bundler') }
+      it { is_expected.to contain_rvm_system_ruby('ruby-3.3.1') }
+      it { is_expected.to contain_group('hdm') }
+      it { is_expected.to contain_user('hdm') }
+      it { is_expected.to contain_vcsrepo('/etc/hdm') }
+      it { is_expected.to contain_file('/etc/hdm/config/hdm.yml') }
+      it { is_expected.to contain_exec('bundle config development') }
+      it { is_expected.to contain_exec('bundle config path') }
+      it { is_expected.to contain_exec('bundle db:setup') }
+      it { is_expected.to contain_exec('bundle install') }
+      it { is_expected.to contain_exec('bundle rails credentials') }
+      it { is_expected.to contain_systemd__unit_file('hdm.service') }
+
+      if os_facts[:os]['family'] == 'RedHat' && os_facts[:os]['release']['major'].to_i < 8
+        it { is_expected.to contain_package('centos-release-scl') }
+        it { is_expected.to contain_package('devtoolset-7') }
+        it { is_expected.to contain_exec('update sqlite') }
+      end
     end
   end
 end
